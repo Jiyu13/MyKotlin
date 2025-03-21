b6836e50266bf190cecda1c0c4ba6624a26b89f8
jiyu13 <jiyucoding@yahoo.com>
2025-03-20 22:06:38 -0400

    create circle + make circle text centered

--- app/src/main/java/com/example/mykotlin/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mykotlin.ui.theme.MyKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +39,32 @@ class MainActivity : ComponentActivity() {
 fun MyApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.primary
+        color = Color(0xFF546E7A)
     ) {
         Text("Hello")
     }
 }
+
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(
+        modifier = Modifier.padding(3.dp).size(45.dp),
+        shape = CircleShape
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                modifier = Modifier,
+                text = "Tap"
+            )
+        }
+
+    }
+}
+
 
 
 @Preview(showBackground = true)
